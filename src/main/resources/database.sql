580eb74d50774aec5d7f346cdcd9359272c112a0
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2025-03-18 10:43:51 +0000

    database template added

---
+++ src/main/resources/database.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS customerdatabase;
+
+USE customerdatabase;
+
+CREATE TABLE IF NOT EXISTS Product (
+
+    );
+
+CREATE TABLE IF NOT EXISTS Orders (
+
+    );
+
+CREATE TABLE IF NOT EXISTS Customer (
+
+    );
+
+CREATE TABLE IF NOT EXISTS Address (
+
+    );
+
+CREATE TABLE IF NOT EXISTS Brands (
+
+    );
+
+CREATE TABLE IF NOT EXISTS Staff (
+
+    );
