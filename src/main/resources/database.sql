c085427de8da03bde430757a43f7d08659f6e5b0
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2025-03-19 13:11:36 +0000

    Tabels started to create

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -3,10 +3,19 @@ CREATE DATABASE IF NOT EXISTS customerdatabase;
 USE customerdatabase;
 
 CREATE TABLE IF NOT EXISTS Product (
+    productID INT(11) NOT NULL AUTO_INCREMENT,
+    productName VARCHAR(50) NOT NULL,
+    productDescription VARCHAR(50) NOT NULL,
+    productPrice INT(11) NOT NULL,
+    warehouseID INT(11) NOT NULL,
+    productBrandID INT(11) NOT NULL,
+    PRIMARY KEY (productID),
+    FOREIGN KEY (warehouseID) REFERENCES Address(productWarehouseID),
 
     );
 
 CREATE TABLE IF NOT EXISTS Orders (
+
 
     );
 
@@ -16,7 +25,8 @@ CREATE TABLE IF NOT EXISTS Customer (
     );
 
 CREATE TABLE IF NOT EXISTS Address (
-
+    customerAddressID INT(11) NOT NULL,
+    productWarehouseID INT(11) NOT NULL,
     );
 
 CREATE TABLE IF NOT EXISTS Brands (
