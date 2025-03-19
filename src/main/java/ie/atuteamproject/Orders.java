a498fbdddf0556139f98306074e81522aa01e853
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-03-19 13:18:57 +0000

    Orders class created

---
+++ src/main/java/ie/atuteamproject/Orders.java
@@ -0,0 +1,52 @@
+package ie.atuteamproject;
+public class Orders
+{
+    private int orderID;
+    private int quantity;
+    private String date;
+    private String dateShipped;
+
+    public int getOrderID()
+    {
+        return orderID;
+    }
+
+
+
+    // test
+    public void setOrderID(int orderID)
+    {
+        this.orderID = orderID;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public String getDateShipped()
+    {
+        return dateShipped;
+    }
+
+    public void setDateShipped(String dateShipped)
+    {
+        this.dateShipped = dateShipped;
+    }
+}
+
