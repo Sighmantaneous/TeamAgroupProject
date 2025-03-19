073bab36e77d9d1a5919441c30c3ab58a2f69b58
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-03-19 13:26:51 +0000

    Updated Orders Class

--- src/main/java/ie/atuteamproject/Orders.java
+++ src/main/java/ie/atuteamproject/Orders.java
@@ -6,47 +6,5 @@ public class Orders
     private String date;
     private String dateShipped;
 
-    public int getOrderID()
-    {
-        return orderID;
-    }
-
-
-
-    // test
-    public void setOrderID(int orderID)
-    {
-        this.orderID = orderID;
-    }
-
-    public int getQuantity()
-    {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity)
-    {
-        this.quantity = quantity;
-    }
-
-    public String getDate()
-    {
-        return date;
-    }
-
-    public void setDate(String date)
-    {
-        this.date = date;
-    }
-
-    public String getDateShipped()
-    {
-        return dateShipped;
-    }
-
-    public void setDateShipped(String dateShipped)
-    {
-        this.dateShipped = dateShipped;
-    }
 }
 
