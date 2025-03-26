bec3403cdd3e86829c5945c5da859d3ea3c99d48
James Donnellan <G00434671@atu.ie>
2025-03-26 21:55:12 +0000

    Starting Update Class and Test

--- src/test/java/testUpdate.java
+++ src/test/java/testUpdate.java
@@ -1,4 +1,21 @@
+import java.sql.*;
+
 public class testUpdate
 {
+    public static void main(String[] args)
+    {
+     String updateSQL = "UPDATE customer SET email = 'newEmail@example.com' WHERE firstName = ''";
 
+    try(Connection connection = DatabaseUtils.getConnection();
+        Statement statement = connection.createStatement())
+    {
+        int rowsUpdated = statement.executeUpdate(UpdateSQL);
+        System.out.println("Rows updated: " + rowsUpdated);
+    }
+    catch (SQLException e)
+        {
+        e.printStackTrace();
+
+        }
+    }
 }
