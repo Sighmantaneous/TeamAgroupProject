fbce79fbf3c20f8cd6a17454aeadab2b95c3c8ed
Simon <g00286398@atu.ie>
2025-03-26 13:38:00 +0000

    Starting insert class and tests

--- src/main/java/TestConnection.java
+++ src/main/java/TestConnection.java
@@ -12,7 +12,7 @@ public class TestConnection {
             // ideally we would use a properties file instead of hardcoding values
             Class.forName("com.mysql.cj.jdbc.Driver");
             // Create a connection to the database, hardcoding values for now.
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampleproducts", "simon", "password");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database", "simon", "password");
             System.out.println("Connection made to connection pool");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
---
+++ src/test/java/testInsert.java
@@ -0,0 +1,45 @@
+
+import org.junit.jupiter.api.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class testInsert {
+
+    InsertData myInsertData;
+
+    @BeforeEach
+    void setup(){
+
+        myInsertData = new InsertData();
+
+    }
+
+    @Test
+    void  TestInsertDataSuccess() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/database", "simon", "password");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sql2 = "Insert into Brand values(?,?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql2)) {
+            stmt.setInt(1, 1);
+            stmt.setString(2, "Nissan");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    void  TestInsertDataFail() {
+        
+    }
+
+}
+
+
