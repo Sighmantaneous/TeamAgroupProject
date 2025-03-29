a1ddaac43182d63733192f630211bc507640498b
Simon <g00286398@atu.ie>
2025-03-29 13:19:20 +0000

    Added layout of my class and added the InsertBrand as its the only one i have a text for currently

--- src/main/java/InsertData.java
+++ src/main/java/InsertData.java
@@ -1,4 +1,119 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class InsertData {
 
 
-}
+    public static void main(String[] args) {
+
+        boolean exit = true;
+
+        Scanner scanner = new Scanner(System.in);
+        Connection connection;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/groupProjectDatabase", "simon", "password");
+            while (exit) {
+                System.out.println("Select table to create Table rows:");
+                System.out.println("1. Address");
+                System.out.println("2. Brand");
+                System.out.println("3. Category");
+                System.out.println("4. Customer");
+                System.out.println("5. Orders");
+                System.out.println("6. Payments");
+                System.out.println("7. Product");
+                System.out.println("8. Warehouse");
+                System.out.println("9. Exit");
+                System.out.print("Enter choice: ");
+                int TableSelect = scanner.nextInt();
+                scanner.nextLine();
+
+
+                switch (TableSelect) {
+
+                    case 1:
+                        //call Address creator
+                        break;
+                    case 2:
+                        //call Brand creator
+                        InsertBrand();
+                        break;
+                    case 3:
+                        //call category creator
+                        break;
+                    case 4:
+                        //call Customer creator
+                        break;
+                    case 5:
+                        //call Order creator
+                        break;
+
+                    case 6:
+                        //call payments creator
+                        break;
+                    case 7:
+                        //call product creator
+                        break;
+                    case 8:
+                        //call warehouse creator
+                        break;
+                    case 9:
+                        exit = false;
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Try again.");
+                        break;
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void InsertBrand() {
+        Scanner scanner = new Scanner(System.in);
+
+         System.out.println("Enter Brand ID: ");
+            int BrandID = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter Brand Name: ");
+            String BrandName = scanner.nextLine();
+
+            CreateBrand(BrandID, BrandName);
+
+
+        }
+
+        public static void CreateBrand(int BrandID, String BrandName) {
+
+            Connection connection;
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost/groupProjectDatabase", "simon", "password");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            String sql2 = "Insert into Brand values(?,?)";
+            try (PreparedStatement stmt = connection.prepareStatement(sql2)) {
+                stmt.setInt(1, BrandID);
+                stmt.setString(2, BrandName);
+                System.out.println("Inserted ID=" + BrandID + " Name="+ BrandName+ " into Brand.");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+           // System.out.println("Inserted " + BrandID + " into Brand.");
+
+
+
+        }
+    }
+
+
--- src/test/java/testInsertData.java
+++ src/test/java/testInsertData.java
@@ -1,45 +1,42 @@
 
 import org.junit.jupiter.api.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
-public class testInsert {
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testInsertData {
 
     InsertData myInsertData;
 
     @BeforeEach
-    void setup(){
+    void setup() {
 
         myInsertData = new InsertData();
 
     }
 
     @Test
-    void  TestInsertDataSuccess() {
+    void TestInsertBrandSuccess() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/database", "simon", "password");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/groupProjectDatabase", "simon", "password");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
-     /*   String sql2 = "Insert into Brand values(?,?)";
+        String sql2 = "Insert into Brand values(?,?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql2)) {
             stmt.setInt(1, 1);
             stmt.setString(2, "Nissan");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-*/
+
     }
 
-    @Test
-    void  TestInsertDataFail() {
-        
     }
 
-}
+
 
 
