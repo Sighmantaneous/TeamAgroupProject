9b83dfaf836799941cfceb948ebebc74c2e76476
James Donnellan <G00434671@atu.ie>
2025-03-31 22:23:38 +0100

    Added Brand to Update class

--- src/main/java/UpdateData.java
+++ src/main/java/UpdateData.java
@@ -11,8 +11,7 @@ public class UpdateData
 
         try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/groupProjectDatabase", "james", "password"))
         {
-            while(!exit)
-            {
+            while(!exit) {
                 System.out.println("Select table to update");
                 System.out.println("1. Address");
                 System.out.println("2. Brand");
@@ -26,7 +25,84 @@ public class UpdateData
                 System.out.print("Enter choice: ");
                 int TableSelect = sc.nextInt();
                 sc.nextLine();
+
+                switch (TableSelect) {
+                    case 1:
+                        updateAddress(connection, sc);
+                        break;
+
+                    case 2:
+                        updateBrand(connection, sc);
+                        break;
+
+                    case 3:
+                        updateCateory(connection, sc);
+                        break;
+
+                    case 4:
+                        updateCustomer(connection, sc);
+                        break;
+
+                    case 5:
+                        updateOrders(connection, sc);
+                        break;
+
+                    case 6:
+                        updatePayments(connection, sc);
+                        break;
+
+                    case 7:
+                        updateProduct(connection, sc);
+                        break;
+
+                    case 8:
+                        updateWarehouse(connection, sc);
+                        break;
+
+                    case 9:
+                        exit = true;
+                        System.out.println("Exiting...");
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                        break;
+                }
             }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void updateBrand(Connection connection, Scanner sc)
+    {
+        System.out.println("Enter Brand ID to update");
+        int brandID = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter new Brand Name");
+        String newBrandName = sc.nextLine();
+
+        String sql = "UPDATE brands SET brand_name = ? WHERE id = ?";
+        try(PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setString(1, newBrandName);
+            statement.setInt(2,brandID);
+            int rowsUpdated = statement.executeUpdate();
+
+            if(rowsUpdated > 0)
+            {
+                System.out.println("Brand updated successfully.");
+            }
+            else
+            {
+                System.out.println("Error updating brand.");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
         }
     }
 }
