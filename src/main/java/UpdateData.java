18ae23f5be1dd833857b71a412b7bd1fc74e1d55
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-04-01 10:30:29 +0100

    Adding Address, Customer and Category to update class

--- src/main/java/UpdateData.java
+++ src/main/java/UpdateData.java
@@ -13,15 +13,15 @@ public class UpdateData
         {
             while(!exit) {
                 System.out.println("Select table to update");
-                System.out.println("1. Address");
-                System.out.println("2. Brand");
-                System.out.println("3. Category");
-                System.out.println("4. Customer");
-                System.out.println("5. Orders");
-                System.out.println("6. Payments");
-                System.out.println("7. Product");
-                System.out.println("8. Warehouse");
-                System.out.println("9. Exit");
+                System.out.println("1, Address");
+                System.out.println("2, Brand");
+                System.out.println("3, Category");
+                System.out.println("4, Customer");
+                System.out.println("5, Orders");
+                System.out.println("6, Payments");
+                System.out.println("7, Product");
+                System.out.println("8, Warehouse");
+                System.out.println("9, Exit");
                 System.out.print("Enter choice: ");
                 int TableSelect = sc.nextInt();
                 sc.nextLine();
