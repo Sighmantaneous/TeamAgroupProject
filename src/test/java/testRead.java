4e575f8154e39d3a02eee1e691db0c94b75d152f
aaron <G00434425@atu.ie>
2025-04-22 13:47:31 +0100

    added read test

---
+++ src/test/java/testRead.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class testRead {
+
+}
